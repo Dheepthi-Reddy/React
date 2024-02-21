90f466b7b1bd215b9de43e1e2fe82bdfaa092e19
Dheepthi-Reddy <144747236+Dheepthi-Reddy@users.noreply.github.com>
2024-02-20 22:09:11 -0600

    types of components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
+// import Greet from './components/Greet'
+import MyComponent from './components/Greet'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
+      {/* <Greet></Greet> */}
       {/* or <Greet/> */}
+      <MyComponent/>
+      <Welcome/>
     </div>
   );
 }
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-// functional component
-function Greet(){
-    return <h1>Hello Dheepthi</h1>
-}
+// ***Functional Component***
+// function Greet(){
+//     return <h1>Hello Dheepthi</h1>
+// }
+const Greet = () => <h1>Hello Dheepthi Reddy</h1>
+
 
 export default Greet
